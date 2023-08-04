d4fc75c37e6b7fd35a97860ec760451fd68d0329
0xHaru <0xharu.git@gmail.com>
2023-08-05 00:06:40 +0200

    Updated fuzzer

--- config/fuzz.c
+++ config/fuzz.c
@@ -6,9 +6,17 @@
 int
 LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
-    CfgEntry entries[64];
-    char err[64 + 1];
-    cfg_parse(Data, Size, entries, 64, err, 64);
+    Cfg cfg;
+    CfgEntry *entries = malloc(256 * sizeof(CfgEntry));
+
+    if (entries == NULL)
+        return 1;
+
+    cfg_init(&cfg, entries, 64);
+
+    char err[MAX_ERR_LEN + 1];
+    int res = cfg_parse(Data, Size, &cfg, err);
+    free(entries);
     return 0;
 }
 
